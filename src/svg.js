e3a1e0bc6b678426082f2c32c1f8ccaac98d75c1
Chris Cross <realchriscrosschaos@gmail.com>
2022-02-07 19:30:48 -0500

    modal alert implemented

--- src/svg.js
+++ src/svg.js
@@ -1,37 +1,91 @@
-window.addEventListener("load", function() {
-    //get svg xml document
-    var svgObject = document.getElementById('mechOutline').contentDocument;
-    //get svg tag as object
-    var mechSVG = svgObject.getElementById('mechSVG');
-    //debug output
+/*classes*/
+class modalAlert {
+    constructor() {
+        this.title = "Alert";
+        this.description = "Processing, please wait..."
+        this.canEscape = false;
+        this.svgObject = null;
+    }
+    static ns = "http://www.w3.org/2000/svg";
+    setTitle(string) {
+        this.title = string;
+    }
+    title() {
+        return this.title;
+    }
+    setDescription(string) {
+        this.description = string;
+    }
+    description() {
+        return this.description;
+    }
+    setCanEscape(boolean) {
+        this.canEscape = boolean;
+    }
+    canEscape() {
+        return this.canEscape;
+    }
+    draw() {
+        this.svgObject = null;
+    }
+}
+/*onLoad*/
+window.addEventListener("load", function () { // Get the modal
+    let modalLayer = document.getElementById("modalAlert");
+    let btn = document.getElementById("myBtn");
+    let closeButton = document.getElementsByClassName("modalClose")[0];
+    let modalSVG = document.getElementById("modalSVG");
+    // Test Button
+    btn.onclick = function () {
+        modalLayer.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+    closeButton.onclick = function () {
+        modalLayer.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      console.log(event.target);
+        if (event.target == modalLayer) {
+            modalLayer.style.display = "none";
+        }
+    }
+    // get svg xml document
+    let svgObject = document.getElementById('mechOutline').contentDocument;
+    // get svg tag as object
+    let mechSVG = svgObject.getElementById('mechSVG');
+    // debug output
     console.log(mechSVG);
-    //add event listener to respond to click
+    // add event listener to respond to click
     mechSVG.addEventListener('pointerdown', function (event) {
         console.log(event.target);
-        if (event.target.closest('stroke')) return;
-        //call toggleComponentGroup
+        if (event.target.closest('stroke')) 
+            return;
+        
+        // call toggleComponentGroup
         deselectComponent(mechSVG);
-        var selectionGroup = event.target.getAttributeNS(null, 'class')
+        let selectionGroup = event.target.getAttributeNS(null, 'class')
         if (selectionGroup != null) {
-          console.log(selectionGroup);
-          var pathGroup = mechSVG.getElementById(selectionGroup);
-          console.log(pathGroup);
-          selectComponent(pathGroup);
+            console.log(selectionGroup);
+            let pathGroup = mechSVG.getElementById(selectionGroup);
+            console.log(pathGroup);
+            selectComponent(pathGroup);
         }
     }, false);
-  });
-  function selectComponent(pathGroup) {
-    var pathArray = pathGroup.getElementsByTagName("path");
-    console.log('selecting '+pathGroup.getAttributeNS(null, 'id'));
+});
+/*functions*/
+function selectComponent(pathGroup) {
+    let pathArray = pathGroup.getElementsByTagName("path");
+    console.log('selecting ' + pathGroup.getAttributeNS(null, 'id'));
     pathGroup.setAttributeNS(null, 'class', 'selected')
     pathArray.item(0).setAttributeNS(null, 'stroke', '#ffffff');
-  }
-  function deselectComponent(mechSVG){
-    var selected = mechSVG.querySelectorAll(".selected");
+}
+function deselectComponent(mechSVG) {
+    let selected = mechSVG.querySelectorAll(".selected");
     if (selected.length > 0) {
-      console.log('deselecting '+selected.item(0).getAttributeNS(null, 'id'));
-      var pathArray = selected.item(0).getElementsByTagName("path");
-      selected.item(0).setAttributeNS(null, 'class', 'enabled')
-      pathArray.item(0).setAttributeNS(null, 'stroke', '#816c3f');
+        console.log('deselecting ' + selected.item(0).getAttributeNS(null, 'id'));
+        let pathArray = selected.item(0).getElementsByTagName("path");
+        selected.item(0).setAttributeNS(null, 'class', 'enabled')
+        pathArray.item(0).setAttributeNS(null, 'stroke', '#816c3f');
     }
-  }
+}
